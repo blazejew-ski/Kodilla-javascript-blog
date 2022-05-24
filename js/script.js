da70306d55bbe56dde4c57bbd285d2a23d0f2e7f
Marcin Błażejewski <macin@blazejew.ski>
2022-05-24 20:35:19 +0200

    added - remove of active class from links & articles

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    console.log('Link was clicked!');
+    console.log(event);
 
-  /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
 
-  /* add class 'active' to the clicked link */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* remove class 'active' from all articles */
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
-  /* get 'href' attribute from the clicked link */
+    /* add class 'active' to the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    /* remove class 'active' from all articles */
 
-  /* add class 'active' to the correct article */
-}
+    const activeArticles = document.querySelectorAll('.posts .active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
+    }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', titleClickHandler);
 }
